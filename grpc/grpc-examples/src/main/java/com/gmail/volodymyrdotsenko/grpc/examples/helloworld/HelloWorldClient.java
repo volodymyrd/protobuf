a7ac6861a39fceb5ee64572e37b74fc2ecce60cf
Volodymyr_Dotsenko <volodymyrdotsenko@gmail.com>
2016-11-09 12:26:34 +0100

    gprc examples -  helloworld

---
+++ grpc/grpc-examples/src/main/java/com/gmail/volodymyrdotsenko/grpc/examples/helloworld/HelloWorldClient.java
@@ -0,0 +1,81 @@
+package com.gmail.volodymyrdotsenko.grpc.examples.helloworld;
+
+import io.grpc.ManagedChannel;
+import io.grpc.ManagedChannelBuilder;
+import io.grpc.StatusRuntimeException;
+
+import java.util.concurrent.TimeUnit;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ * A simple client that requests a greeting from the {@link HelloWorldServer}.
+ */
+public class HelloWorldClient {
+
+    private static final Logger logger = Logger.getLogger(HelloWorldClient.class.getName());
+
+    private final ManagedChannel channel;
+    private final GreeterGrpc.GreeterBlockingStub blockingStub;
+
+    /**
+     * Construct client connecting to HelloWorld server at {@code host:port}.
+     */
+    public HelloWorldClient(String host, int port) {
+        channel = ManagedChannelBuilder.forAddress(host, port)
+            // Channels are secure by default (via SSL/TLS). For the example we disable TLS to avoid
+            // needing certificates.
+            .usePlaintext(true)
+            .build();
+        blockingStub = GreeterGrpc.newBlockingStub(channel);
+    }
+
+    public void shutdown() throws InterruptedException {
+        channel.shutdown().awaitTermination(5, TimeUnit.SECONDS);
+    }
+
+    /**
+     * Say hello to server.
+     */
+    public void greet(String... name) {
+        logger.info("Will try to greet ...");
+
+        for (String n : name) {
+            sendRequest(n);
+        }
+    }
+
+    private void sendRequest(String name) {
+        HelloRequest request = HelloRequest.newBuilder().setName(name).build();
+        HelloReply response;
+        try {
+            response = blockingStub.sayHello(request);
+            logger.info("Greeting: " + response.getMessage());
+            response = blockingStub.sayHelloAgain(request);
+            logger.info("Greeting again: " + response.getMessage());
+        } catch (StatusRuntimeException e) {
+            logger.log(Level.WARNING, "RPC failed: {0}", e.getStatus());
+            return;
+        }
+    }
+
+    /**
+     * Greet server. If provided, the first element of {@code args} is the name to use in the
+     * greeting.
+     */
+    public static void main(String[] args) throws Exception {
+        HelloWorldClient client = new HelloWorldClient("localhost", 50051);
+        try {
+            /* Access a service running on the local machine on port 50051 */
+            String user = "world";
+            if (args.length > 0) {
+                user = args[0]; /* Use the arg as the name to greet if provided */
+                client.greet(args);
+            } else {
+                client.greet(user);
+            }
+        } finally {
+            client.shutdown();
+        }
+    }
+}
